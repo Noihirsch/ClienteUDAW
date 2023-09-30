f6df090fc4c5a0fc7f03acd05dde88da3de3d995
Noihirsch <10dianagoncalves@gmail.com>
2023-09-30 20:58:51 +0200

    exercise 1 and 2 unit 3

--- UNIDAD3/U3E1/script.js
+++ UNIDAD3/U3E1/script.js
@@ -3,20 +3,8 @@
 
 //Crea un programa que muestre el número de días que quedan desde hoy hasta el fin de curso (por ejemplo, el 22 de junio).
 
-
-const tiempoActual = Date.now();
-const hoy = new Date(date1);
-
-const fin = new Date('Jun 22 2024');
-
-console.log(hoy);
-console.log(fin);
-function calcularFinDeCurso(hoy, fin) {
-    let dias = ((fin.getTime()) - (hoy.getTime()));
-    dias = ((dias / 86400000));
-    console.log(dias);
-
-    return dias;
-  }
-
-calcularFinDeCurso();
+let hoy = new Date();
+var fin = new Date("2024/06/22");
+dif = fin.getTime() - hoy.getTime();
+var dif = Math.ceil(dif / (1000 * 3600 * 24));
+alert("Quedan " + dif + " días para el fin de curso");
---
+++ UNIDAD3/U3E2/script.js
@@ -0,0 +1,11 @@
+//Crea un programa que pida el día y el mes de tu cumpleaños y 
+//muestre los años en que tu cumpleaños va a caer en domingo
+ //desde el año actual hasta el año 2100.
+
+ function birthdaysOnSunday{
+
+ var dia = document.getElementById("dia").value;
+ var mes = document.getElementById("mes").value;
+ var hoy= new Date();
+ 
+ }
