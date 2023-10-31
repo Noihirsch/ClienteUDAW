7b81f1376459218f0509260ca83db95b89843d22
noihirsch <10dianagoncalves@gmail.com>
2023-10-31 11:00:07 +0100

    progress made, still not functional

--- UNIDAD4/U4E4/disco.js
+++ UNIDAD4/U4E4/disco.js
@@ -1,5 +1,4 @@
-
-  export default class Disco {
+     class Disco {
     constructor(nombre, interprete, anyo, genero, localizacion = 0, prestado = false, caratula = 'imagen.png') {
         this.nombre = nombre;
         this.interprete = interprete;
@@ -22,8 +21,37 @@
     let info = `Disco: ${this.nombre} Interprete: ${this.interprete} Año: ${this.color} Género ${this.genero} Localización: ${this.localizacion} Prestado ${this.prestado} Caratula: ${this.caratula}`;
     document.getElementById("infoDisco").innerHTML = info;
 }
+    }
+
+    let disco1= new Disco("Black Holes and Revelations", "Muse", "2006", "Rock");
+    let disco2= new Disco("The Resistance", "Muse", "2009", "Rock");
+    const discoteca = [disco1, disco2];
+function countDiscos(){
+    let countres = discoteca.length;
+    document.getElementById("countres").innerHTML = countres;
 }
 
-let d1 = new Disco("Black Holes and Revelations", "Muse", "2006", "Rock");
-let d2 = new Disco("The Resistance", "Muse", "2009", "Rock");
-let discoteca = [d1, d2];
+function fillTabla(){
+    const tableBody = document.querySelector("#tabla tbody");
+    if (tableBody) {
+      tableBody.innerHTML = discoteca.map(disco => `<tr><td>${disco.nombre}</td><td>${disco.interprete}</td><td>${disco.anyo}</td><td>${disco.genero}</td><td>${disco.localizacion}</td><td>${disco.prestado}</td><td>${disco.caratula}</td></tr>`).join('');
+    } else {
+      console.error('Table body not found.');
+    }
+}
+
+function showTabla() {
+  const tableBody = document.querySelector("#tabla tbody");
+  tableBody.classList.toggle('tabla');
+}
+
+function disksInBetween() {
+  let anyo1 = document.getElementById('anyo1').value;
+  let anyo2 = document.getElementById('anyo2').value;
+  let anyosInBetween = discoteca.filter(disco => disco.anyo > anyo1 && disco.anyo < anyo2);
+  if (anyosInBetween) {
+      const tableBody = document.querySelector("#tabla tbody");
+      tableBody.innerHTML = anyosInBetween.map(disco => `<tr><td>${disco.nombre}</td><td>${disco.interprete}</td><td>${disco.anyo}</td></tr>`).join('');
+  }
+}
+
