8337359f3ed70fbcb83cadbda72f4e7e6e47d562
noihirsch <10dianagoncalves@gmail.com>
2023-11-10 14:32:05 +0100

    exercise 2 done.

---
+++ UNIDAD5/U5E2/script.js
@@ -0,0 +1,9 @@
+var saludarCadaTanto = setInterval(comenzarSaludos, 3000);
+
+function comenzarSaludos(){
+let saludar = alert("Hola!");
+}
+
+function pararSaludos(){
+    clearTimeout(saludar);
+}
