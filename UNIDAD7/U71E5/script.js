2cb89231511c5847830a0bca743242a9ed54b389
Noihirsch <10dianagoncalves@gmail.com>
2024-01-30 22:29:04 +0100

    u71-u75

--- UNIDAD7/U71E5/script.js
+++ UNIDAD7/U71E5/script.js
@@ -1,23 +1,58 @@
-import Tarea from "./Tarea";
-window.addEventListener("load", inicio);
 
 AllTareas = [];
 
-function inicio() {
-    document.getElementById("add").addEventListener("click", añadirTarea);
+const lista = document.querySelector("#misTareas");
+
+
+
+class Tarea{
+    constructor(texto){
+        this.nombre= texto;
+        this.realizada = false;
+    }
+
+    add(){
+        const todoInput=document.querySelector("#myInput").value;
+        if (todoInput ==""){
+            alert("Tarea vacía. Inténtalo de nuevo.");
+        } else {
+            const t1 = new Tarea(todoInput);
+        }
+        AllTareas.unshift(t1);
+        this.display;
+        document.querySelector("#myInput").value= '';
+
+    }
+
+    done(i){
+        const index = AllTareas.findIndex((item)=> item.id ==x);
+        console.log(AllTareas[index].realizada);
+        AllTareas[index].realizada == false ? 
+        AllTareas[index].realizada = true : AllTareas[index].realizada = false;
+        this.display();
+
+    }
+
+    delete(j){
+        const index = AllTareas.findIndex((item) => item.id == z);
+        AllTareas.splice(index, 1);
+        this.display();
+
+    }
+
+    display(){
+        this.ulElement.innerHTML = "";
+
+        AllTareas.forEach(item => {
+
+            const li = document.createElement("li");
+            const borrar = document.createElement("i");
+
+            li.innerText = item.nombre;
+            li.setAttribute("data-id", item.realizada);
+            
+            borrar.setAttribute("data-")
+        });
+
+    }
 }
-
-function añadirTarea () {
-    var tareaInput = document.getElementById("task");
-    let newTarea = new Tarea(tareaInput.value);
-
-    var texto = document.createTextNode(document.getElementById("<ul>dsd</ul>").value);
-    parrafo.appendChild(texto);
-
-
-    tareaInput.value = "";
-}
-
-
-
-
