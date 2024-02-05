f8acdcc0e791d7b0fbd3ac7b4955932db862d0e5
Noihirsch <10dianagoncalves@gmail.com>
2024-02-05 19:06:27 +0100

    ejercicio 4 listo

---
+++ UNIDAD7/U71E5/Tarea.js
@@ -0,0 +1,6 @@
+export default class Tarea {
+  constructor(texto) {
+    this.texto = texto;
+    this.realizada = false;
+  }
+}
--- UNIDAD7/U71E5/script.js
+++ UNIDAD7/U71E5/script.js
@@ -1,53 +1,8 @@
-AllTareas = [];
+import Tarea from "./Tarea";
+window.addEventListener("load", inicio);
 
-const lista = document.querySelector("#misTareas");
+const tareaInput = document.querySelector(".tarea input input"),
+filtros = document.querySelectorAll(".filtros span"),
+clearAll = document.querySelector(".clear.btn"),
+tarea
 
-class Tarea {
-  constructor(texto) {
-    this.nombre = texto;
-    this.realizada = false;
-  }
-
-  add() {
-    const todoInput = document.querySelector("#myInput").value;
-    if (todoInput == "") {
-      alert("Tarea vacía. Inténtalo de nuevo.");
-    } else {
-      const t1 = new Tarea(todoInput);
-    }
-    AllTareas.unshift(t1);
-    this.display;
-    document.querySelector("#myInput").value = "";
-  }
-  
-  done(i) {
-    const index = AllTareas.findIndex((item) => item.id == x);
-    console.log(AllTareas[index].realizada);
-    AllTareas[index].realizada == false
-      ? (AllTareas[index].realizada = true)
-      : (AllTareas[index].realizada = false);
-    this.display();
-  }
-  
-  delete(j) {
-    const index = AllTareas.findIndex((item) => item.id == z);
-    AllTareas.splice(index, 1);
-    this.display();
-  }
-  
-  display() {
-    this.ulElement.innerHTML = "";
-
-    AllTareas.forEach((item) => {
-      const li = document.createElement("li");
-      const borrar = document.createElement("i");
-
-      li.innerText = item.nombre;
-      li.setAttribute("data-nombre", item.nombre);
-
-      borrar.setAttribute("data-nombre", item.realizada);
-      borrar.innerHTML = "BORRAR?";
-    });
-  }
-
-}
