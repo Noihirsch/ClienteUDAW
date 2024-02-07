1dd3e25d8f1bdf24aacf1844af1e30cd60cf5d12
Noihirsch <10dianagoncalves@gmail.com>
2024-02-07 21:23:42 +0100

    ex715 done halfway

--- UNIDAD7/U71E5/script.js
+++ UNIDAD7/U71E5/script.js
@@ -1,8 +1,48 @@
-import Tarea from "./Tarea";
-window.addEventListener("load", inicio);
 
-const tareaInput = document.querySelector(".tarea input input"),
-filtros = document.querySelectorAll(".filtros span"),
-clearAll = document.querySelector(".clear.btn"),
-tarea
+const input = document.querySelector("input");
+const botonAniadir = document.querySelector(".add-button");
+const ul = document.querySelector("ul");
+const empty = document.querySelector(".empty");
 
+allMyTareas = [];
+
+botonAniadir.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(1);
+
+  const nombreTarea = input.value;
+
+  if (nombreTarea !== "") {
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    p.textContent = nombreTarea;
+
+    li.appendChild(p);
+    li.appendChild(addBorrar());
+    ul.appendChild(li);
+
+    let t1 = new Tarea(nombreTarea.value);
+    allMyTareas.push(t1);
+    input.value = "";
+    empty.style.display = "none";
+  }
+});
+
+function addBorrar() {
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "X";
+  deleteButton.className = "delete-button";
+
+  deleteButton.addEventListener("click", (e) => {
+    const item = e.target.parentElement;
+    ul.removeChild(item);
+
+    const items = document.querySelectorAll("li");
+
+    if (items.length === 0) {
+      empty.style.display = "block";
+    }
+  });
+
+  return deleteButton;
+}
